5c6323be2531ed9ef3676c3a35e7fe5b79124a4e
lorenzo <thelorenzo533@gmail.com>
2021-03-05 14:56:49 +0100

    Add PDF viewer

--- UberScan/Controllers/AuthorController.cs
+++ UberScan/Controllers/AuthorController.cs
@@ -43,13 +43,6 @@ namespace UberScan.Controllers
             return View();
         }
 
-        public IActionResult ViewPdf()
-        {
-            
-            return View();
-        }
-
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
